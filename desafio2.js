c2fa0cbf9b38d6a1e080b156ccccd890b1163276
Joaquin <joaquinsangiorgi2002@gmail.com>
2022-10-20 00:16:44 -0300

    Arreglando bug

--- desafio2.js
+++ desafio2.js
@@ -18,13 +18,13 @@ class Contenedor {
 
                     if (result.length > 0) {
 
-                        let lastId = result[result.length - 1].id+1;
+                        let lastId = result[result.length-1].id+1;
                         let productoNuevo = {
                             id: lastId,
                             ...producto
                         }
 
-                        result.push(productoNuevo, null, 2);
+                        result.push(productoNuevo);
                         await fs.promises.writeFile(this.archivo, JSON.stringify(result, null, 2));
                         return lastId;
 
@@ -66,13 +66,13 @@ let contenedor = new Contenedor("productos.txt")
 
 metodos = async() => {
 
-await contenedor.save({title: "Café", price: 123, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"})
+console.log(await contenedor.save({title: "Café", price: 123, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"}))
 
-await contenedor.save({title: "Cafetera", price: 678, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"})
+console.log(await contenedor.save({title: "Cafetera", price: 678, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"}))
     
-await contenedor.save({title: "Capsula de café", price: 456, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"})
+console.log(await contenedor.save({title: "Capsula de café", price: 456, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"}))
     
-await contenedor.save({title: "Filtro de café", price: 345, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"})
+console.log(await contenedor.save({title: "Filtro de café", price: 345, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"}))
 
 }
 
