ad840c2c2dc4c401af82691389065c12450e0106
Joaquin <joaquinsangiorgi2002@gmail.com>
2022-10-22 18:32:33 -0300

    Cambios

--- desafio2.js
+++ desafio2.js
@@ -60,6 +60,78 @@ class Contenedor {
 
     }
 
+getAll = async() => {
+    try {
+
+        if (fs.existsSync(this.archivo)){
+            let info = await fs.promises.readFile(this.archivo, "utf-8");
+            let result = JSON.parse(info)
+                if (result.length > 0){
+                    return result;
+                }else{
+                    return "No hay productos en el contenedor"
+                }
+        }else {
+            return "No hay un archivo creado =("
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getById = async(id) => {
+
+    
+    try {
+        if (fs.existsSync(this.archivo)){
+            let info = await fs.promises.readFile(this.archivo, "utf-8");
+            let result = JSON.parse(info)
+            const infoId = result.filter(producto => producto.id === id) 
+            console.log("Se encontró el siguiente producto")
+            return infoId
+                
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+deleteAll = async() => {
+
+    if (fs.existsSync(this.archivo)){
+        let info = await fs.promises.readFile(this.archivo, "utf-8");
+        let result = JSON.parse(info)
+
+        console.log("Se borraron todos los datos del archivo")
+        return result = []
+        
+    }
+
+}
+
+deleteById = async (id) =>{
+    try {
+
+        if (fs.existsSync(this.archivo)){
+            let info = await fs.promises.readFile(this.archivo, "utf-8");
+            let result = JSON.parse(info)
+            if(result.some(producto => producto.id === id)){
+                const eliminando = result.filter(producto => producto.id !== id)
+                await fs.promises.writeFile(this.archivo, eliminando)
+                console.log("Producto eliminado")
+                return result
+                
+            }
+
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 }
 
 let contenedor = new Contenedor("productos.txt")
@@ -72,8 +144,13 @@ console.log(await contenedor.save({title: "Cafetera", price: 678, thumbnail: "ht
     
 console.log(await contenedor.save({title: "Capsula de café", price: 456, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"}))
     
-console.log(await contenedor.save({title: "Filtro de café", price: 345, thumbnail: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fs1.eestatic.com%2F2017%2F02%2F13%2Fcocinillas%2Fcocinillas_193495389_116293001_1706x960.jpg&imgrefurl=https%3A%2F%2Fwww.elespanol.com%2Fcocinillas%2Frecetas%2Fbebidas%2F20171006%2Fhacer-cafe-perfecto-ciencia%2F1001008949096_30.html&tbnid=EjkHHh0zSoVDkM&vet=12ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ..i&docid=KsYYB05ZfJwBBM&w=1706&h=960&q=cafe&client=firefox-b-lm&ved=2ahUKEwjQgvLZw-36AhUSu5UCHezECjQQMygBegUIARDjAQ"}))
+console.log(await contenedor.getAll())
 
+console.log(await contenedor.getById(2))
+
+console.log(await contenedor.deleteAll())
+
+console.log(await contenedor.deleteById(1))
 }
 
 metodos()
